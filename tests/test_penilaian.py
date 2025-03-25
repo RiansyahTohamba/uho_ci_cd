23a25247645b6ac4c54fe0860c2fa75de3e13fcc
mriansyah93 <mriansyah93@gmail.com>
2025-03-25 11:31:19 +0800

    fail test

--- tests/test_penilaian.py
+++ tests/test_penilaian.py
@@ -6,9 +6,10 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))
 from models import Penilaian
 
 def test_hitung_nilai_akhir():
-    normal = Penilaian(80, 70, 90, 85)
+    normal = Penilaian(tugas=80, uts=70, uas=90, proyek=85)
     hard = Penilaian(100, 100, 100, 100)
     easy = Penilaian(0, 0, 0, 0)
-    assert normal.hitung_nilai_akhir() == (80 * 0.1) + (70 * 0.2) + (90 * 0.2) + (85 * 0.5), "Perhitungan salah untuk mode normal"
+    expected_normal_output = (80 * 0.1) + (70 * 0.2) + (90 * 0.2) + (85 * 0.5)
+    assert normal.hitung_nilai_akhir() == expected_normal_output, "Perhitungan salah untuk mode normal"
     assert hard.hitung_nilai_akhir() == 100, "Perhitungan salah untuk mode hard"
     assert easy.hitung_nilai_akhir() == 0, "Perhitungan salah untuk mode easy"
